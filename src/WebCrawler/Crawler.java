b24adaa080b75c40d423044828c79e7dcf337fd9
Andreea Frincu <frincu60@gmail.com>
2020-12-07 15:31:28 +0200

    Implementare schelet Crawler
    Issue: #3
    Implementează scheletul clasei Crawler
    
    Signed-off-by: Andreea Frincu <frincu60@gmail.com>

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -1,4 +1,30 @@
 package WebCrawler;
 
+import java.util.ArrayList;
+
 public class Crawler {
+
+    private static Crawler instance = null;
+    private UserData arguments;
+    private Threads multithread;
+    private ArrayList<String> linkList;
+    private ArrayList<Visited> visitedLinks;
+
+    private Crawler() {
+        this.arguments = arguments;
+        this.multithread = multithread;
+        this.linkList = linkList;
+        this.visitedLinks = visitedLinks;
+    }
+
+    public void run() {
+
+    }
+
+    public static Crawler getInstance() {
+        if (instance == null)
+            instance = new Crawler();
+
+        return instance;
+    }
 }
