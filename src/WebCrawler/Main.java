25e2613d0a82c791f138ca2131ba124c94cce9ec
Marginean Florin <florin.marginean1998.11@gmail.com>
2020-12-09 22:35:00 +0200

    Implementare metoda parse()
    Issue: #13
    Implementeaza metoda parse() din
    clasa ParserPage
    
    Signed-off-by: Marginean Florin <florin.marginean1998.11@gmail.com>

--- src/WebCrawler/Main.java
+++ src/WebCrawler/Main.java
@@ -2,9 +2,13 @@ package WebCrawler;
 
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.print("Hello World!");
+        //ParserPage p=new ParserPage("https://wikimediafoundation.org/about/financial-reports//about/jobs");
+        //p.parse();
+        //p.getLinkList();
     }
 }
