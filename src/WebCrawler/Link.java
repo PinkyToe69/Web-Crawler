2fb2e60024a5af29f185040a640d0a3528df6632
andrei-comandasu <andreitcom@yahoo.com>
2020-12-12 10:40:27 +0200

    Implementare clasa link.
    Issue: #17
    Functionalitati clasa link.
    
    Signed-off-by: andrei-comandasu <andreitcom@yahoo.com>

--- src/WebCrawler/Link.java
+++ src/WebCrawler/Link.java
@@ -1,4 +1,26 @@
 package WebCrawler;
 
 public class Link {
+
+    private string content,currentLinkName,parrentLink;
+    private ArrayList<String>childrenLinks;
+    public Link(string content,string currentLinkName,string parrentLink,ArrayList<String>childrenLinks){
+        this.content=content;
+        this.currentLinkName=currentLinkName;
+        this.parrentLink=parrentLink;
+        this.childrenLinks=childrenLinks;
+    }
+    public string getLinkName(){
+        return currentLinkName;
+    }
+    public string getParrentLink(){
+        return parrentLink;
+    }
+    public ArrayList<String> getChildrenLink(){
+        return childrenLinks;
+    }
+    public string getContent(){
+        return content;
+    }
+
 }
