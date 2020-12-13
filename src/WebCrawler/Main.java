64358a2eca227ff3cacb83215c934146339d0199
Liviu Floristean <liviu.floristean@mta.ro>
2020-12-13 11:47:43 +0200

    Parsare Multithreaded
    Issue: #21
    Programul parseaza pe mai multe threaduri
    fiecare pagina primita ca argument
    pe fiecare nivel de adancime
    
    Signed-off-by: Liviu Floristean <liviu.floristean@mta.ro>

--- src/WebCrawler/Main.java
+++ src/WebCrawler/Main.java
@@ -2,9 +2,16 @@ package WebCrawler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
+        Crawler application = Crawler.getInstance();
+        application.run();
 
+    //    ParserPage p = new ParserPage("https://mta.ro/");
+    //    p.parse();
+    //    p.getLinkList();
+    //    p.getLinkContent();
     }
 }
--- src/WebCrawler/Threads.java
+++ src/WebCrawler/Threads.java
@@ -7,37 +7,64 @@ public class Threads extends Thread{
     private String currentLink;
     private ParserPage parsedList;
     private ArrayList<Link> listOfNodes;
-    private ArrayList<String> finalListofURLs;
+    private ArrayList<String> childLinks;
+    private boolean done = false;
 
     public Threads(String linkPassed)
     {
         currentLink = linkPassed;
+        parsedList = new ParserPage(currentLink);
+        listOfNodes = new ArrayList<Link>();
+        childLinks = new ArrayList<String>();
     }
-
 
     public ArrayList<String> getFinalList()
     {
-        return finalListofURLs;
+        return childLinks;
     }
 
+    public boolean getStatus()
+    {
+        return done;
+    }
+
+    @Override
     public void run()
     {
        //   to insert try-catch later...
     //    int numberOfURLs = urlList.size();
         ArrayList<String> content = new ArrayList<String>();
-        ArrayList<String> childLinks = new ArrayList<String>();
 
         //   parsedList = new ParserPage(urlList.get(i));
         // won't work without try-catch, we're gonna solve this with the exception class
         try {
             parsedList.parse();
-            childLinks.addAll(parsedList.getLinkList());       // save the links we discover//    content.addAll(parsedList.get_urlContent());            // save the content - need to teamwork for that
-            /**
-             * Here we're gonna need the Link class to add the content and the link list for the current node (link)
-             */
+            childLinks.addAll(parsedList.getLinkList());        // save the links we discover
+            content.addAll(parsedList.getLinkContent());        // get the content of the link (png, jpg, etc.)
+
+        //    for(int i = 0; i<childLinks.size(); i++)
+        //    {
+        //        System.out.println("Thread "+ Thread.currentThread().getId() +
+        //                " found the following links on link " + currentLink);
+        //        System.out.println(childLinks.get(i));
+        //        System.out.println();
+        //     }
+        //    System.out.println("Content size is: " + content.size());
+        //    System.out.println("Thread "+ Thread.currentThread().getId() +
+        //            " found the following content on link " + currentLink);
+        //    for(int j = 0; j<content.size(); j++)
+        //    {
+        //        System.out.println(content.get(j));
+        //    }
+        //    System.out.println("============================================");
+
+
+            //  Here we're gonna need the Link class to add the content and the link list for the current node (link)
+            //  ... to do ...
+
+            done = true;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        finalListofURLs.addAll(childLinks);         // we create the new list of url for the next depth level
     }
 }
