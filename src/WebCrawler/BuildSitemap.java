a6a41c2a686a7438040fe7bcc56e748fd1aa3c38
adina-ciobanu-selab <adina.ciobanu@mta.ro>
2020-12-13 16:37:15 +0200

    Implementare functionalitate creareSitemap
    Issue #23
    Implementare creare fisier sitemap
    Signed-off-by: adina-ciobanu-selab <adina.ciobanu@mta.ro>

--- src/WebCrawler/BuildSitemap.java
+++ src/WebCrawler/BuildSitemap.java
@@ -1,7 +1,75 @@
 package WebCrawler;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class BuildSitemap {
-    
+    private String sitemapFilename;
+    private ArrayList<Link> visitedLinks;
+
+    public BuildSitemap(String sitemapFilename, Visited visited) {
+        this.sitemapFilename = sitemapFilename;
+        this.visitedLinks=visited.getListOfNodes();
+    }
+
+    private Link getChild(String name){
+        for(int i = 0; i < visitedLinks.size(); i++){
+            if(visitedLinks.get(i).getLinkName() == name){
+                return visitedLinks.get(i);
+            }
+        }
+        return null;
+    }
+
+    private void writeMe(Link root, FileWriter fileWriter, int tabs) throws IOException {
+        if(root == null){
+            return;
+        }
+        for(int i = 0; i < tabs; i++){
+            fileWriter.write('\t');
+        }
+        fileWriter.write(root.getLinkName());
+        fileWriter.write('\n');
+        ArrayList<String> childrenNames = root.getChildrenLink();
+        if(childrenNames == null){
+            return;
+        }
+        for(String childName : childrenNames){
+            Link child = getChild(childName);
+            writeMe(child, fileWriter, tabs + 1);
+        }
+    }
+
+    public void createSitemap(){
+        try {
+            File file = new File(sitemapFilename);
+            if (file.createNewFile()) {
+                //System.out.println("File created: " + myObj.getName());
+                try {
+                    FileWriter fileWriter = new FileWriter(sitemapFilename);
+                    int tabs = 0;
+                    Link root = null;
+                    for(int i = 0; i < visitedLinks.size(); i++){
+                        if(visitedLinks.get(i).getParrentLink() == null) {
+                            root = visitedLinks.get(i);
+                            break;
+                        }
+                    }
+                    writeMe(root, fileWriter, 0);
+
+                    fileWriter.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/WebCrawler/Main.java
+++ src/WebCrawler/Main.java
@@ -7,8 +7,38 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         Crawler application = Crawler.getInstance();
-        application.run();
+        //application.run();
 
+        ArrayList<String> rootChildren = new ArrayList<>();
+        rootChildren.add("docs");
+        rootChildren.add("js");
+        Link l1 = new Link("c","mta.ro",null, rootChildren);
+        ArrayList<String> docsChildren = new ArrayList<>();
+        docsChildren.add("2019");
+        docsChildren.add("2020");
+        Link l2 = new Link("c", "docs", "root", docsChildren);
+        Link l3 = new Link("c", "2019", "docs", null);
+        ArrayList<String> _2020children = new ArrayList<>();
+        _2020children.add("img1.jpg");
+        _2020children.add("prezentare.html");
+        Link l4 = new Link("c", "2020", "docs", _2020children);
+        ArrayList<String> jsChildren = new ArrayList<>();
+        jsChildren.add("index.js");
+        Link l5 = new Link("c", "js", "root", jsChildren);
+        Link l6 = new Link("c", "index.js", "js", null);
+
+        ArrayList<Link> vis = new ArrayList<>();
+        vis.add(l1);
+        vis.add(l2);
+        vis.add(l3);
+        vis.add(l4);
+        vis.add(l5);
+        vis.add(l6);
+        Visited visited = new Visited(vis);
+
+        BuildSitemap buildSitemap = new BuildSitemap("sitemap.txt", visited);
+
+        buildSitemap.createSitemap();
     //    ParserPage p = new ParserPage("https://mta.ro/");
     //    p.parse();
     //    p.getLinkList();
