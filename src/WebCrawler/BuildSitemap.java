785297bad48ed9696a7fb0078048a01b0b472c26
adina-ciobanu-selab <adina.ciobanu@mta.ro>
2020-12-13 14:40:45 +0200

    Implementare-clasa-Visited
    Issue #24
    Creare clasa Visited in care se tine evidenta link-urilor deja parcurse
    
    Signed-off-by: adina-ciobanu-selab <adina.ciobanu@mta.ro>

--- src/WebCrawler/BuildSitemap.java
+++ src/WebCrawler/BuildSitemap.java
@@ -1,4 +1,7 @@
 package WebCrawler;
 
+import java.util.ArrayList;
+
 public class BuildSitemap {
+    
 }
--- src/WebCrawler/Visited.java
+++ src/WebCrawler/Visited.java
@@ -1,4 +1,23 @@
 package WebCrawler;
 
+import java.util.ArrayList;
+
 public class Visited {
+    private ArrayList<Link> listOfNodes;
+
+    public Visited(ArrayList<Link> listOfNodes) {
+        this.listOfNodes = listOfNodes;
+    }
+
+    public ArrayList<Link> getListOfNodes() {
+        return listOfNodes;
+    }
+
+    public void appendList(Link newLink){
+        if(listOfNodes.contains(newLink))
+            return;
+        else {
+            listOfNodes.add(newLink);
+        }
+    }
 }
