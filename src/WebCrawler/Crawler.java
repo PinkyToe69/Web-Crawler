d57ef8a339f60861d712231d88f3807f4cdd4b20
Liviu Floristean <liviu.floristean@mta.ro>
2020-12-20 19:28:23 +0200

    Rezolvare bug#2 multithreading
    Issue: #54
    S-a rezolvat un bug care nu permitea parsarea a mai multe
    url-uri transmise ca parametru prin url.txt
    
    Signed-off-by: Liviu Floristean <liviu.floristean@mta.ro>

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import java.util.concurrent.Semaphore;
 
-public class Crawler {
+public class Crawler{
 
     private static Crawler instance = null;
     private UserData arguments;
@@ -25,6 +25,41 @@ public class Crawler {
         this.parser = new ParserFile();
     }
 
+    /**
+     * Metoda care va rula aplicatia
+     *
+     * <p>Va prelua link-uril primite prin fisierul de intrare si adancimea
+     * in functie de care va continua sa parseze link-urile. Functia foloseste
+     * un semafor pentru a porni maxim n (unde n este dat in fisierul de configurare)
+     * thread-uri pentru executia in paralel a programului</p>
+     *
+     * <p>Aplicatia va lua fiecare url dat in fisierul de configurare si va
+     * parsa toate link-urile pe care le gaseste in continutul acestuia.
+     * Acest lucru se va repeta pana cand se atinge nivelul de adancime permis.</p>
+     *
+     * <p>Parcurgerea se va face multithreaded, in functie de parametrul primit.
+     * Pentru radacina se apeleaza start apoi join succesiv deoarece se stie ca
+     * este nodul radacina si are un singur url. In cazul copiilor, se va crea o
+     * lista de thread-uri, se vor porni, iar executia acestora se va face dupa ce
+     * s-a atins numarul maxim de thread-uri in lista. Copiii returnati vor suprascrie
+     * lista initiala de url-uri, astfel incat dupa ce se parcurg toate url-urile de
+     * nivelul curent, se va avea formata toata lisat de pe nivelul urmator.</p>
+     *
+     * <p>Obiectul logger va afisa mesaje catre untilizator cu fiecare actiune
+     * intreprinsa de aplicatie.</p>
+     *
+     * <p>Dupa fiecare parsare se va lua un obiect de tip Link in care s-au salvat
+     * toate datele despre url-ul parsat. Acel obiect va fi folosit de obiectul
+     * visitedLinks pentru a valida unicitatea nodului.</p>
+     *
+     * <p>In final, se foloseste obiectul buildSitemap pentru crearea sitemap-ului.</p>
+     *
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+
     public Crawler run() throws IOException, URISyntaxException, InterruptedException {
         parser.parse();
         arguments = new UserData(parser);
@@ -32,7 +67,6 @@ public class Crawler {
         this.logger = new LogManager(arguments.getLogLevel());
 
 
-        //============================
         int nrOfLinksPassed = linkList.size();
         int depth = arguments.getDepthLevel();
         limitSemaphore=new Semaphore(arguments.getNoOfThreads());
@@ -40,56 +74,55 @@ public class Crawler {
         System.out.println("The depth is " + depth);
         System.out.println(nrOfLinksPassed + " links had been passed");
 
-        for(int link = 0; link < nrOfLinksPassed; link++)       // we take every link passed as argument
+        for(int link = 0; link < nrOfLinksPassed; link++)       // luam fiecare link dat ca parametru
         {
             ArrayList<String> linksOnTheCurrentLevel = new ArrayList<String>();
             ArrayList<String> linksOnThePreviousLevel = new ArrayList<String>();
             ArrayList<Link> listOfNodes = new ArrayList<Link>();
 
-            for (int i = 0; i < depth; i++)
-            {
-                if (i == 0)      // if we're on the first level
+            for (int i = 0; i < depth; i++) {
+                if (i == 0)                                                     // daca suntem pe primul nivel
                 {
-                    limitSemaphore.acquire();   // blocks a permit
-                    linksOnTheCurrentLevel.add(linkList.get(i));                // we get the first argument link
-                    multithread = new Threads(linksOnTheCurrentLevel.get(i));   // it will receive just the first element
+                    limitSemaphore.acquire();                                   // blockeaza (se ia un permit)
+                    linksOnTheCurrentLevel.add(linkList.get(link));                // luam primul link dat ca argument
+                    multithread = new Threads(linksOnTheCurrentLevel.get(i));   // se va lua doar primul link pentru ca stim ca e radacina
                     multithread.start();
+                    System.out.println("Starting parsing " + multithread.getCurrentLink() + " on thread " + multithread.getId());
                     multithread.join();
                     node = multithread.getNode();
                     visitedLinks.appendList(node);
 
-                    linksOnTheCurrentLevel.clear();                             // we rewrite the list
+                    linksOnTheCurrentLevel.clear();                             // rescriem lista cu copiii gasiti de radacina
                     linksOnTheCurrentLevel.addAll(multithread.getFinalList());
-                    limitSemaphore.release();   // release a permit
-
-                }
-                else {
-                    int nrOfNecessaryThreads;        // number of necessary threads
+                    limitSemaphore.release();                                   // permite continuarea executiei (se pune un permit)
+                } else {
+                    int nrOfNecessaryThreads;                                   // numarul de thread-uri necesare (un thread / un link)
                     ArrayList<Threads> listOfThreads = new ArrayList<Threads>();
                     nrOfNecessaryThreads = linksOnTheCurrentLevel.size();
+
+                    int activeThreads = 0;
 
                     linksOnThePreviousLevel.addAll(linksOnTheCurrentLevel);
 
                     for (int j = 0; j < nrOfNecessaryThreads; j++) {
                         if (limitSemaphore.availablePermits() > 0) {
-                            limitSemaphore.acquire();   // blocks a permit
+                            limitSemaphore.acquire();                           // blockeaza (se ia un permit)
                             multithread = new Threads(linksOnThePreviousLevel.get(j));
                             listOfThreads.add(multithread);
                             multithread.start();
-                            logger.writeMessage(multithread.getCurrentLink());////////////////////////////
-                            System.out.println("Starting parsing " + multithread.getCurrentLink());
-                            //    multithread.join();
+                            activeThreads++;
+                            logger.writeMessage(multithread.getCurrentLink());
+                            System.out.println("Starting parsing " + multithread.getCurrentLink() + " on thread " + multithread.getId());
                         } else {
-                            for (int k = 0; k < arguments.getNoOfThreads(); k++) {
-                                logger.writeMessage(listOfThreads.get(k).getCurrentLink());/////////////////////
-                                System.out.println("Parsing " + listOfThreads.get(k).getCurrentLink());
+                            for (int k = 0; k < activeThreads; k++) {
+                                logger.writeMessage(listOfThreads.get(k).getCurrentLink());
+                                System.out.println("Parsing " + listOfThreads.get(k).getCurrentLink() + " on thread " + listOfThreads.get(k).getId());
                                 listOfThreads.get(k).join();
                                 node = listOfThreads.get(k).getNode();
                                 listOfNodes = visitedLinks.getListOfNodes();
 
-                                for(int o = 0; o<listOfNodes.size(); o++)
-                                {
-                                    if(node != null) {
+                                for (int o = 0; o < listOfNodes.size(); o++) {
+                                    if (node != null) {
                                         if (listOfNodes.get(o).getLinkName() != node.getParrentLink()) {
                                             visitedLinks.appendList(node);
                                         }
@@ -97,9 +130,10 @@ public class Crawler {
                                 }
 
                                 linksOnThePreviousLevel.addAll(listOfThreads.get(k).getFinalList());  // extended list
-                                limitSemaphore.release();   // release a permit
+                                limitSemaphore.release();                          // permite continuarea executiei (se pune un permit)
                             }
                             listOfThreads.clear();
+                            activeThreads = 0;
                         }
                     }
                 }
--- src/WebCrawler/Threads.java
+++ src/WebCrawler/Threads.java
@@ -5,12 +5,50 @@ import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Aceasta clasa este alcatuita doar din membrii privati care
+ * alcatuiesc continutul unui url. Mosteneste clasa Thread pentru
+ * functionalitatea multithreading a aplicatiei.
+ *
+ * <p>Se suprascrie functia run, unde se apeleaza un metodele
+ * din clasa ParserPage. Auxiliar, se foloseste un mutex pentru
+ * a evita situatiile de conflict intre thread-uri</p>.
+ *
+ * <p>Dupa ce se parseaza pagina, toate datele sunt salvate intr-un
+ * obiect de tip Link</p>.
+ *
+ * @author Liviu Floristean
+ * @see ParserPage
+ * @see Link
+ */
+
 public class Threads extends Thread {
+    /**
+     * <b>currentLink</b> reprezinta numele url-ului care se parseaza acum.
+     *
+     * <b>parsedList</b> este un obiect de tip ParserPage care parseaza
+     * pagina cu url-ul primit.
+     *
+     * <b>node</b> este un obiect de tip Link in care se salveaza toate
+     * datele obtinute in urma parsarii.
+     *
+     * <b>childLinks</b>> este o lista de obiecte de tip String cu care
+     * se salveaza toate link-urile gasite in url-ul curent.
+     *
+     * <b>executionMutex</b> reprezinta un mutex prin care se asigura
+     * excluziunea mutuala in functia run().
+     *
+     */
     private String currentLink;
     private ParserPage parsedList;
     private Link node;
     private ArrayList<String> childLinks;
     private final Lock executionMutex;
+
+    /**
+     * Constructorul clasei Threads.
+     * @param linkPassed este link-ul care trebuie parsat.
+     */
 
     public Threads(String linkPassed) {
         currentLink = linkPassed;
@@ -19,18 +57,49 @@ public class Threads extends Thread {
         executionMutex = new ReentrantLock(false);
     }
 
+    /**
+     * Aceasta metoda este folosita pentru a lua link-ul
+     * care este parsat acum.
+     * @return va returna numele link-ului.
+     */
     public String getCurrentLink() {
         return currentLink;
     }
+
+    /**
+     * Aceasta metoda este folosita pentru a se lua lista
+     * de link-uri copii in urma parsarii.
+     * @return va da o lista care va contine fiecare copil.
+     */
 
     public ArrayList<String> getFinalList() {
         return childLinks;
     }
 
+    /**
+     * Aceasta metoda este folosita pentru a lua nodul obtinut
+     * cu elementele fiecarui link.
+     * @return va returna un obiect de tip Link.
+     */
+
     public Link getNode() {
         return node;
     }
 
+    /**
+     * Aceasta metoda este suprascrisa pentru a putea fi folosita
+     * prin thread-uri.
+     *
+     * <p>Se foloseste mutexul pentru excluziunea mutuala. Fiecare
+     * thread va apela metodele obiectului ParserPage, dupa care isi
+     * va salva lista de copii si continutul gasit.</p>
+     *
+     * <p>Prin datele obtinute din obiectul ParserPage, thread-ul va
+     * construi un obiect de tip Link in care va salva tot ce a gasit
+     * in link-ul transmis ca parametru.</p>
+     *
+     * <p>Metoda abordeaza mecanisme de tratare a exceptiilor.</p>
+     */
     @Override
     public void run() {
         ArrayList<String> content = new ArrayList<String>();
